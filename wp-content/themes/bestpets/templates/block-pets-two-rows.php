e9682340fcef12dabfe98e9fa373ee5f45519ff3
Yevhenii Sukach <sukachy@digicode.ua>
2023-05-17 17:52:02 +0300

    save in progress

--- wp-content/themes/bestpets/templates/block-pets-two-rows.php
+++ wp-content/themes/bestpets/templates/block-pets-two-rows.php
@@ -9,40 +9,61 @@ $args = array(
             'terms' => 'pets'
         )
     ),
-    'posts_per_page' => 8
+    'posts_per_page' => 8,
+    'order' => 'ASC',
+    'orderby' => 'modified'
 );
 
 $pets = get_posts($args);
-
-
-foreach ($pets as $pet) {
-
-    $product_id = $pet->ID;
-    $product = wc_get_product($product_id);
-
-    $gene = $product->get_attribute('gene');
-    $age = $product->get_attribute('age');
-
-    $product_image = get_the_post_thumbnail($product_id);
-    $product_title = $pet->post_title;
-    $link = get_permalink($product_id);
-
-
 ?>
-    <div>
-        <?php
-        if (!empty($product_image)) {
-            echo $product_image;
-        }
-        ?>
-    </div>
-<?php
 
-    echo '<a href="' . $link . '">' . $product_title . '</a>';
+<div class="pets-grid-4-2">
 
-    // var_dump($pet);
-}
+    <?
+    foreach ($pets as $pet) {
+
+        $product_id = $pet->ID;
+        $product = wc_get_product($product_id);
+
+        $gene = $product->get_attribute('gene');
+        $age = $product->get_attribute('age');
+        $_SKU = $product->get_sku();
+        $price = $product->get_price();
+        $formatted_price = number_format($price, 0, ',', '.');
+
+        $product_image = get_the_post_thumbnail($product_id);
+        $product_title = $pet->post_title;
+        $link = get_permalink($product_id);
+
+        $add_to_cart_url = $product->add_to_cart_url();
+        $add_to_cart_text = $product->add_to_cart_text();
 
 
+    ?>
+        <div class="pet-single-product-card">
+            <div class="pet-single-product-card-image-container">
+                <?php
+                if (!empty($product_image)) {
+                    echo $product_image;
+                }
+                ?>
+            </div>
+            <?php
 
-?>
+            echo '<a class="pet-name" href="' . $link . '">' . $_SKU . ' - ' . $product_title . '</a>';
+            echo '<div class="pet-attributes">Gene: <span class="bold padding-left-5">' . $gene . 
+            '</span><span class="padding-left-10 padding-right-10">&#8226</span>Age: <span class="bold padding-left-5">' 
+            . $age . '</span></div>';
+            
+            echo '<div class="bold pet-price-container">' . $formatted_price . ' UAH <a href="' . esc_url($add_to_cart_url) . '" data-quantity="1" class="take-button">TAKE</a></div>';
+
+            echo '';
+
+            // var_dump($pet);
+            ?>
+        </div>
+    <?php
+    }
+    ?>
+
+</div>
